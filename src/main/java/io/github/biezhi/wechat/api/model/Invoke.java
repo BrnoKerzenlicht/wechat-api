18022acd0f654224130116f72a95bde8f54527ff
BrnoKerzenlicht <bjliuyue1@corp.netease.com>
2019-01-25 20:03:41 +0800

    MOD:invoke方法调整

--- src/main/java/io/github/biezhi/wechat/api/model/Invoke.java
+++ src/main/java/io/github/biezhi/wechat/api/model/Invoke.java
@@ -3,17 +3,11 @@ package io.github.biezhi.wechat.api.model;
 import io.github.biezhi.wechat.WeChatBot;
 import io.github.biezhi.wechat.api.enums.AccountType;
 import io.github.biezhi.wechat.api.enums.MsgType;
-import io.github.biezhi.wechat.exception.WeChatException;
+import java.lang.reflect.Method;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 消息执行器
@@ -26,7 +20,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 @AllArgsConstructor
 public class Invoke {
 
-    private static final Set<String> INVOKED_MSG = new HashSet<>();
+//    private static final Set<String> INVOKED_MSG = new HashSet<>();
 
     private Method            method;
     private List<AccountType> accountTypes;
@@ -40,9 +34,9 @@ public class Invoke {
      * @param <T>
      */
     public <T extends WeChatBot> void call(T bot, WeChatMessage message) {
-        if (INVOKED_MSG.contains(message.getId())) {
-            return;
-        }
+//        if (INVOKED_MSG.contains(message.getId())) {
+//            return;
+//        }
         try {
             // 自动同意添加好友
             if (message.getMsgType() == MsgType.ADD_FRIEND && bot.config().autoAddFriend()) {
@@ -51,17 +45,17 @@ public class Invoke {
             }
             Account account = bot.api().getAccountById(message.getFromUserName());
             if (null == account) {
-                INVOKED_MSG.add(message.getId());
+//                INVOKED_MSG.add(message.getId());
                 method.invoke(bot, message);
                 return;
             }
             if (msgType == MsgType.ALL || msgType == message.getMsgType()) {
                 if (message.getMsgType() == MsgType.ADD_FRIEND) {
-                    INVOKED_MSG.add(message.getId());
+//                    INVOKED_MSG.add(message.getId());
                     method.invoke(bot, message);
                 } else {
                     if (accountTypes.contains(account.getAccountType())) {
-                        INVOKED_MSG.add(message.getId());
+//                        INVOKED_MSG.add(message.getId());
                         method.invoke(bot, message);
                     }
                 }
